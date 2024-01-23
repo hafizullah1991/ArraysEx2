3706893fd3e1f4e47ee1acd7b5528abb1ea823e9
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-22 19:00:18 -0800

    exercise done

---
+++ ArraylistExercise/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO:
+
+
+
+            // Create an int array and populate it with numbers 1-10  Done      
+            int[] populate = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            /* Create a list of type int
+             
+             * Name the list "evens"
+             */
+            List<int> Evens = new List<int>();
+
+
+
+            // Create another list of type int, name the list "odds"
+            List<int> Odds = new List<int>();
+
+            // Iterate through the array to separate even and odd numbers
+            foreach (int i in populate)
+            {
+                if (i % 2 == 0)
+                {
+                    Evens.Add(i);
+                }
+                else
+                {
+                    Odds.Add(i);
+                }
+            }
+
+            // Display even numbers using a foreach loop
+            Console.WriteLine("Even Numbers:");
+            foreach (int even in Evens)
+            {
+                Console.WriteLine(even);
+            }
+
+            // Display odd numbers using a foreach loop
+            Console.WriteLine("\nOdd Numbers:");
+            foreach (int odd in Odds)
+            {
+                Console.WriteLine(odd);
+            }
+        }
+    }
+}
